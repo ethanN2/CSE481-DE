38f51293592a581951999b1eb9b15ea93857e0cc
datproad126 <datngodoan123@gmail.com>
2020-07-01 17:23:10 +0700

    reset password

--- routes/api.php
+++ routes/api.php
@@ -19,6 +19,15 @@ Route::group(['prefix' => 'auth'], function () {
     Route::group(['middleware' => 'auth:api'], function () {
         Route::get('logout', 'UserController@logout');
     });
+    Route::group([
+        'namespace' => 'auth',
+        'prefix' => 'password',
+        'middleware' => ['auth:api']
+    ], function () {
+        Route::post('create', 'PasswordResetController@create');
+        Route::get('find/{token}', 'PasswordResetController@find');
+        Route::post('reset', 'PasswordResetController@reset');
+    });
 });
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
