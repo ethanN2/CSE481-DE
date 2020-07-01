634dbb244967e7ee74b3fa9b08f1dce72a04a351
datproad126 <datngodoan123@gmail.com>
2020-07-01 18:05:21 +0700

    fix migration passwrod reset

--- database/migrations/2014_10_12_100000_create_password_resets_table.php
+++ database/migrations/2014_10_12_100000_create_password_resets_table.php
@@ -17,7 +17,7 @@ class CreatePasswordResetsTable extends Migration
             $table->increments('id');
             $table->string('email')->index();
             $table->string('token');
-            $table->timestamp('created_at')->nullable();
+            $table->timestamps();
         });
     }
 
