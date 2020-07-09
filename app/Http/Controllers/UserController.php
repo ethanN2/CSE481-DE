7f7b890b3a01c23edf57bbf3d0a4cb4bed7ac530
ken <datngodoan123@gmail.com>
2020-07-09 14:59:32 +0700

    update UserController

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\User;
-// use App\Notifications\RegisterActivate;
+use App\Notifications\RegisterActivate;
 use App\Mail\mailForm;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
