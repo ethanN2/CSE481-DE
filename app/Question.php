caeeae8b25ab87d3316b47a89d0e7c9852b7b21a
ken <datngodoan123@gmail.com>
2020-07-20 23:31:56 +0700

    update model question

--- app/Question.php
+++ app/Question.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class Question extends Model
 {
     protected $table = "questions";
-    protected $fillable = ['question'];
+    protected $fillable = ['question', 'level'];
 
     /**
      * Get the answers for the blog post.
