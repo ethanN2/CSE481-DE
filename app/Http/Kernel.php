0dd47968cb6203613a690d21bb87740ce5808cb5
ken <datngodoan123@gmail.com>
2020-07-23 00:24:01 +0700

    fixing error cors

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -52,7 +52,6 @@ class Kernel extends HttpKernel
      * @var array
      */
     protected $routeMiddleware = [
-        'api' => ['api/*'],
         'auth' => \App\Http\Middleware\Authenticate::class,
         'auth.basic' => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
         'bindings' => \Illuminate\Routing\Middleware\SubstituteBindings::class,
