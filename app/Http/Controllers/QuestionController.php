b7e7204cce056d7262bb0b9542804676c151a277
ken <datngodoan123@gmail.com>
2020-07-23 14:13:24 +0700

    update questioncontroller

--- app/Http/Controllers/QuestionController.php
+++ app/Http/Controllers/QuestionController.php
@@ -3,73 +3,61 @@
 namespace App\Http\Controllers;
 
 use App\Question;
+use Illuminate\Validation\Rule;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Validator;
 
 class QuestionController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function index(Request $request)
     {
-        if (!$request->get('per_page')) {
-            return response()->json([
-                'message' => 'need per_page to fetch!!',
-                'hint' => 'set per_page = 10 || 15 || 20 ||25'
-            ]);
+        $request->validate([
+            'per_page' => 'required|integer',
+            'page_number' => 'required|integer'
+        ]);
+
+        $defaultPagination = [10, 15, 20, 25];
+        foreach ($defaultPagination as $key => $value) {
+            if ($request->get('per_page') == $value) {
+                $questions = Question::paginate($request->get('per_page'), ['*'], 'page', $request->get('page_number'));
+                return response()->json([
+                    'data' => $questions,
+                    'message' => 'get data successfully!'
+                ]);
+            }
         }
-        switch ($request->get('per_page')) {
-            case 10:
-                $questions = Question::paginate(10);
-                break;
-            case 15:
-                $questions = Question::paginate(15);
-                break;
-            case 20:
-                $questions = Question::paginate(20);
-                break;
-            case 25:
-                $questions = Question::paginate(25);
-                break;
-            default:
-                $questions = Question::paginate(10);
-            break;
-        }
+
         return response()->json([
-            'data' => $questions,
-            'message' => 'get data successfully!'
+            'message' => 'set per_page = 10 || 15 || 20 || 25'
         ]);
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
     public function store(Request $request)
     {
-        if (!$request->get('question'))
-            return response()->json([
-                'message' => 'need require quesition'
-            ], 404);
-        if (!$request->get('level'))
-            return response()->json([
-                'message' => 'need require level'
-            ], 404);
-        $question = new Question([
-            'question' => $request->get('question'),
-            'level' => $request->get('level')
+        $request->validate([
+            'question' => 'required|string',
+            'level' => 'required|string',
         ]);
-        $question->save();
-        return response()->json([
-            'data' => $question,
-            'message' => 'create data succcessfully!'
-        ])
-            ->withCallback($request->input('callback'));
+
+        $defaultLevel = ['easy', 'medium', 'hard', 'expert'];
+        foreach ($defaultLevel as $key => $value) {
+            if ($request->get('level') == $value) {
+                $question = new Question([
+                    'question' => $request->get('question'),
+                    'level' => $request->get('level')
+                ]);
+
+                $question->save();
+                return response()->json([
+                    'data' => $question,
+                    'message' => 'create data succcessfully!'
+                ])
+                    ->withCallback($request->input('callback'));
+            }
+        }
+
+        return response()->json(['message' => 'level only set 4 values "easy", "medium", "hard", "expert"'],400);
     }
 
     /**
@@ -88,41 +76,53 @@ class QuestionController extends Controller
         ]);
     }
 
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $question_id
-     * @return \Illuminate\Http\Response
-     */
     public function update(Request $request, $question_id)
     {
+        $request->validate([
+            'question' => 'required|string',
+            'level' => 'required|string'
+        ]);
+
         $question = Question::findorfail($question_id);
+        
+        $defaultLevel = ['easy', 'medium', 'hard', 'expert'];
+        foreach ($defaultLevel as $key => $value) {
+            if ($request->get('level') == $value) {
+                $question->question = $request->get('question');
 
-        $question->question = $request->get('question');
+                $question->save();
+        
+                return response()->json([
+                    'data' => $question,
+                    'message' => 'update question successfully!'
+                ])
+                    ->withCallback($request->input('callback'));;
+            }
+        }
 
-        $question->save();
-
-        return response()->json([
-            'data' => $question,
-            'message' => 'update question successfully!'
-        ])
-            ->withCallback($request->input('callback'));;
+        return response()->json(['message' => 'level only set 4 values: ["easy", "medium", "hard", "expert"]'],400);
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $question_id
-     * @return \Illuminate\Http\Response
-     */
     public function destroy($question_id)
     {
         $question = Question::findorfail($question_id);
         $question->delete();
         return response()->json([
             'message' => 'deleted question!'
+        ]);
+    }
+
+    public function search(Request $request)
+    {
+        $request->validate([
+            'query' => 'required'
+        ]);
+
+        $query = Question::where('quesiton', 'LIKE', '%'.$request->get('query').'%');
+
+        return response()->json([
+            'data' => $query,
+            'message' => 'find question successfully!!'
         ]);
     }
 }
